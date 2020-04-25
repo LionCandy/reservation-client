f2bfe437acf619db5c4e867e6083d7874922fe9c
Emanuel BG <barreragz.emanuel@gmail.com>
2020-04-25 18:53:56 -0500

    base spring code

---
+++ src/main/java/nio/bg/workshop/reservationclient/ReservationClientApplication.java
@@ -0,0 +1,13 @@
+package nio.bg.workshop.reservationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nio/bg/workshop/reservationclient/ReservationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package nio.bg.workshop.reservationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
